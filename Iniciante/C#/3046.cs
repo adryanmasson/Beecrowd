af153f8f42f3b79b42a6e5d05042214af64a6959
Adryan Masson <99740516+adryanmasson@users.noreply.github.com>
2023-05-24 20:31:09 -0300

    Create 3046.cs

---
+++ Iniciante/C#/3046.cs
@@ -0,0 +1,13 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int N = Convert.ToInt32(Console.ReadLine());
+        
+        Console.WriteLine((N+1) * (N+2) / 2);
+
+    }
+
+}
